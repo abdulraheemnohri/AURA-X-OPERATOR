31abb356d989341b7e8a8bf6409dfd3827768d28
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-08-13 19:08:36 +0500

    feat: add v3 control center shell

---
+++ app/src/main/java/com/aurax/operator/ui/screens/V3ControlCenterScreen.kt
@@ -0,0 +1,7 @@
+package com.aurax.operator.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun V3ControlCenterScreen() { Text("AURA-X v3 Control Center") }
