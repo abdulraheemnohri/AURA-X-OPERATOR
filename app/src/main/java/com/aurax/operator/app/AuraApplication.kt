fe51cdc68491cb63f101b3966cc23ce2f7ddf661
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-08-13 11:06:02 +0500

    fix: enable Hilt application component on main

--- app/src/main/java/com/aurax/operator/app/AuraApplication.kt
+++ app/src/main/java/com/aurax/operator/app/AuraApplication.kt
@@ -3,9 +3,12 @@ package com.aurax.operator.app
 import android.app.Application
 import com.aurax.operator.data.database.AuraDatabase
 import com.aurax.operator.operator.OperatorAudit
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class AuraApplication : Application() {
     val db by lazy { AuraDatabase.get(this) }
+
     override fun onCreate() {
         super.onCreate()
         OperatorAudit.init(this)
