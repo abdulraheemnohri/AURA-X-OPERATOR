511adc1a5c1d9973ade1f2f68bc6bc0eddd068b8
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-08-13 11:05:11 +0500

    fix: remove stale Hilt module imports

--- app/src/main/java/com/aurax/operator/di/AppModule.kt
+++ app/src/main/java/com/aurax/operator/di/AppModule.kt
@@ -1,12 +1,8 @@
 package com.aurax.operator.di
 
 import android.content.Context
-import com.aurax.operator.agent.planner.LocalModelPlanner
-import com.aurax.operator.agent.planner.OperatorPlanner
 import com.aurax.operator.data.database.AuraDao
 import com.aurax.operator.data.database.AuraDatabase
-import com.aurax.operator.data.export.SafetyLogExporter
-import com.aurax.operator.agent.execution.TaskExecutor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -24,21 +20,4 @@ object AppModule {
     @Provides
     @Singleton
     fun provideDao(database: AuraDatabase): AuraDao = database.dao()
-
-    @Provides
-    @Singleton
-    fun providePlanner(): OperatorPlanner = OperatorPlanner()
-
-    @Provides
-    @Singleton
-    fun provideLocalModelPlanner(@ApplicationContext context: Context): LocalModelPlanner = LocalModelPlanner(context)
-
-    @Provides
-    @Singleton
-    fun provideTaskExecutor(
-        @ApplicationContext context: Context,
-        database: AuraDatabase,
-        planner: OperatorPlanner,
-        localModelPlanner: LocalModelPlanner
-    ): TaskExecutor = TaskExecutor(context, database, planner, localModelPlanner)
 }
