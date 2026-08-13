0f6b7d758efd428d9d50678a9bad0758cab25fc7
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-08-13 11:01:15 +0500

    fix: add Hilt database bindings

---
+++ app/src/main/java/com/aurax/operator/di/AppModule.kt
@@ -0,0 +1,23 @@
+package com.aurax.operator.di
+
+import android.content.Context
+import com.aurax.operator.data.database.AuraDao
+import com.aurax.operator.data.database.AuraDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AuraDatabase = AuraDatabase.get(context)
+
+    @Provides
+    @Singleton
+    fun provideDao(database: AuraDatabase): AuraDao = database.dao()
+}
