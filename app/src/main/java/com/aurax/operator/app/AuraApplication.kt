4430c6f75fc9007f995aeb5cda66d86729f6279f
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-08-13 16:03:20 +0500

    feat: seed model hub catalog on startup

--- app/src/main/java/com/aurax/operator/app/AuraApplication.kt
+++ app/src/main/java/com/aurax/operator/app/AuraApplication.kt
@@ -1,16 +1,27 @@
 package com.aurax.operator.app
 
 import android.app.Application
+import com.aurax.operator.ai.model.ModelHub
 import com.aurax.operator.data.database.AuraDatabase
 import com.aurax.operator.operator.OperatorAudit
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 @HiltAndroidApp
 class AuraApplication : Application() {
     val db by lazy { AuraDatabase.get(this) }
 
+    @Inject lateinit var modelHub: ModelHub
+
+    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
     override fun onCreate() {
         super.onCreate()
         OperatorAudit.init(this)
+        applicationScope.launch { modelHub.seedBuiltIns() }
     }
 }
